5d484d68150fa6494cd716d14f4ecab7f3cc25c6
GeoTsinganos <georgets786@gmail.com>
2026-01-26 18:00:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 set -x
 
 python --version
-python manage.py makemigrations --check --dry-run || true
-python manage.py showmigrations bookings || true
+
+python manage.py showmigrations bookings
+python manage.py makemigrations bookings --check --dry-run
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 exec gunicorn booking_system.wsgi:application --bind 0.0.0.0:${PORT}
